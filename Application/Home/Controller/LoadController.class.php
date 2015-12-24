cddb96aba990479cad4ab890eddb37d72dfa3289
taoking <980512523@qq.com>
2015-12-24 18:09:57 +0800

    上传
    上传基础

--- Application/Home/Controller/LoadController.class.php
+++ Application/Home/Controller/LoadController.class.php
@@ -1,8 +1,25 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Think\Upload;
 class LoadController extends Controller {
     public function index(){
         $this->display('index');
+    }
+    
+    
+    public function upload(){
+       $upload = new \Think\Upload();
+       $upload->maxSize = 3333333;
+       $upload->exts = array('jpg','gif','png','jpeg');
+       $upload->savePath= "Public/Uploads/";
+       $info = $upload->upload();
+       if (!$info){
+           $this->error($upload->getError());
+       }else{
+           $this->success("上传成功");
+       }
+       
+       
     }
 }
